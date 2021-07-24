9d15014e91cb0b7abc5a2c996ce5bdfdd880b3af
Miguel <miguel.danto@gmail.com>
2021-07-24 00:52:31 -0500

    first commit

---
+++ cuentas/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.utils.timezone import now as now_for_default
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Account(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    user = models.ForeignKey(User, models.DO_NOTHING, related_name='accounts')
+    balance = models.FloatField(default=0)
+    type_account = models.CharField(max_length=20, choices=[("normal","Normal"),("debt","Debt")],default="normal")
+    created = models.DateTimeField(default=now_for_default)
+    
+
+class Transaction(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    amount = models.FloatField(default=0)
+    account = models.ForeignKey(Account, models.DO_NOTHING,related_name="transactions")
+    occurred_in = models.DateTimeField(default=now_for_default)
+    created = models.DateTimeField(default=now_for_default)
+    type_transaction = models.CharField(max_length=20, choices=[("add","Add"),("subtract","Subtract")])
+
